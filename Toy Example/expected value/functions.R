e161608dab28cfe7598d1eb32a69b12eaefcb77f
Andee Kaplan <ajkaplan@iastate.edu>
2014-12-18 20:15:12 -0600

    workng on min_radius

--- Toy Example/expected value/functions.R
+++ Toy Example/expected value/functions.R
@@ -1,9 +1,7 @@
-
 stats <- function(H, V, type="negative") {
   if(type == "binary") t <- data.frame(c(0,1))[,rep(1, H + V)]
   else if(type == "negative") t <- data.frame(c(-1,1))[,rep(1, H + V)]
-  else if(type == "experiment") t <- data.frame(c(1,3))[,rep(1, H + V)]
-  else stop("Type must be 'binary', 'negative', or 'experiment'")
+  else stop("Type must be 'binary', 'negative'")
   
   names(t) <- c(paste0("h", 1:H), paste0("v", 1:V))  
   
@@ -17,31 +15,42 @@ stats <- function(H, V, type="negative") {
     }
   }
   
-  return(t.grid)
+  return(data.matrix(t.grid))
 }
 
-expected_value <- function(stats, theta, normalized = TRUE) {
-  if(normalized) result <- t(as.matrix(stats)) %*% exp(as.matrix(stats) %*% as.matrix(theta)) %*% diag(1/apply(exp(as.matrix(stats) %*% as.matrix(theta)), 2, sum))
-  else result <- t(as.matrix(stats)) %*% exp(as.matrix(stats) %*% as.matrix(theta))
-  
+expected_value <- function(theta, stats, normalized = TRUE) {
+  result <- crossprod(t(crossprod(stats, exp(crossprod(t(stats), theta)))), diag(1/apply(exp(crossprod(t(stats), theta)), 2, sum)))
   rownames(result) <- paste0("exp_", rownames(result))
   return(result)
 }
 
-sample_sphere <- function(space, n, r = 1) {
-  return(data.frame(sapply(1:n, function(i){
+sample_sphere <- function(stat, n, r = 1) {
+  require(dplyr)
+  require(tidyr)
+  
+  samp <- function(space, r) {
     vec <- rnorm(space)
-    r*vec/sqrt(sum(vec^2))
-  })))  
+    paste0(r*vec/sqrt(sum(vec^2)), collapse=",")
+  }
+  
+  res <- data.frame(samp = 1:n) %>%
+    group_by(samp) %>%
+    mutate(vals = samp(ncol(stat), r)) %>%
+    separate(col = vals, sep = ",", into = colnames(stat)) %>%
+    select(-samp) %>%
+    data.matrix() %>%
+    t()
+    
+  
+  return(res)  
 }
 
-calc_hull <- function(stats) {
+calc_hull <- function(stats, options="FA") {
   #calculate convex hull
   require(geometry)
-  C <- convhulln(stats, options="FA")
+  C <- convhulln(stats, options=options)
   return(C)
 }
-
 
 generate_hull_from_stats <- function(stats) {
   ## stat is the df of possible statistics as generated from stats
@@ -57,19 +66,19 @@ generate_hull_from_stats <- function(stats) {
   return(lines)
 }
 
-
 min_distance_to_hull <- function(x0, lines) {
   ## x0 is a single point (numeric vector)
   ## lines is a list of lines connecting all the points in the statistic space as generated from generate_hull_from_stats
-  
-  min(unlist(lapply(lines, function(line){
+
+  x0 <- unlist(x0)
+  data.frame(min_dist = min(unlist(lapply(lines, function(line){
     x1 <- as.numeric(line$x1)
     x2 <- as.numeric(line$x2)
     
     rej <- (x1 - x0) - (sum((x1 - x0)*(x2-x1))/sum((x2-x1)^2))*(x2 - x1)
     
     sqrt(sum(rej^2))    
-  })))
+  }))))
 }
 
 project_to_sphere <- function(sphere, point) {
@@ -77,5 +86,46 @@ project_to_sphere <- function(sphere, point) {
   r <- sphere$radius
   center <- sphere$center  
   return((point - center)*r/sqrt(sum((point-center)^2)) + center  )
+}
+
+distance_to_simplex <- function(simplex, P) {
+  require(dplyr)
+  require(tidyr)
+  ## P is a point in H + V + H*V space
+  ## simplex a list of three points that make a triangle 
+  ## algorithm implemented from http://data-science-radio.com/pubs/2012-eccad-distance-to-simplex.pdf
+  
+  d <- length(simplex) - 1
+
+  simplex_copy <- simplex
+  P_copy <- P
+
+  P <- as.numeric(strsplit(P, ",")[[1]])
+  simplex <- simplex %>% unlist %>%
+    data.frame() %>%
+    rename(point=simplex.....unlist) %>%
+    separate(point, paste0("coord", 1:length(P)), sep=",") %>%
+    data.matrix()
+  
+  if(d == 0) return(dist(matrix(c(P, simplex), nrow=2, byrow=TRUE)))
+  
+  ## translate to that S0 is the origin
+  P <- P - simplex[1,]
+  simplex <- sweep(simplex, 2, simplex[1,], "-")[-1,]
+  
+  ## project onto the linear subspace
+  rows <- expand.grid(1:nrow(simplex), 1:nrow(simplex))
+  alpha <- solve(matrix(apply(rows, 1, function(x){ crossprod(simplex[x[1],], simplex[x[2],])}), nrow=nrow(simplex)), apply(simplex, 1, crossprod, P))
+  P0 <- colSums(alpha*simplex)
+  
+  if(sum(alpha) <= 1 & sum(alpha < 0) == 0) {
+    return(dist(matrix(c(P, P0), nrow=2, byrow=TRUE)))
+  } else if(sum(alpha < 0) > 0) {
+    simplex0 <- simplex_copy[c(1, 1+ which(alpha > 0))]
+    distance_to_simplex(P_copy, simplex0)    
+  } else {
+    distance_to_simplex(P_copy, simplex_copy[-1]) 
+  }
+  
 }
 
--- Toy Example/expected value/min_radius.R
+++ Toy Example/expected value/min_radius.R
@@ -4,44 +4,81 @@ library(reshape2)
 library(ggplot2)
 
 ## constants
-n <- 1000 #num points to sample on the sphere
+n <- 500 #num points to sample on the sphere
+cutoff <- .01
+step <- 1
+backstep <- .01
 
-min_radius <- function(H, V, type='negative', n) {
+test_cases <- expand.grid(data.frame(1:4)[,rep(1,2)]) %>% 
+  rename(H = X1.4, V = X1.4.1) %>%
+  filter(H <= V) %>% #remove those cases with less visibles than hiddens. Is this necessary?
+  mutate(n_param = H*V + H + V) %>%
+  mutate(max_facets = (2^(H+V))^(floor(n_param/2))) %>%
+  filter(n_param <= 11) %>% #can't handle any higher dimensions currently
+  rowwise() %>%
+  mutate(min_radius(H, V, n=n, cutoff=cutoff, backstep=backstep, step=step))
+
+min_radius <- function(H, V, type='negative', n, cutoff, backstep, step) {
   stat <- stats(H, V, type)
-  lines <- generate_hull_from_stats(stat)
+  
+  h <- calc_hull(stat, options="Qt")
+  triangles <- h %>%
+    data.frame() %>%
+    rowwise() %>%
+    do(data.frame(stat[unlist(.),])) %>%
+    ungroup() %>%
+    transmute_(point=paste0("paste(",paste(colnames(stat), collapse=","),", sep=',')")) %>%
+    mutate(triangle=rep(1:(n()/3), each = 3)) %>%
+    mutate(point_num=paste0("point", rep(1:3, (n()/3)))) %>%
+    dcast(triangle ~ point_num, value.var = "point")
+  
   
   ## inits
   r0 <- 0
-  theta0 <- sample_sphere(space = ncol(stat), n, r0)
+  theta0 <- sample_sphere(stat, n, r0)
   exp_vals0 <- expected_value(stats = stat, theta = theta0, normalized = TRUE)
-  d0 <- min(apply(exp_vals0, 2, function(x) min_distance_to_hull(x, lines)))
-  step0 <- 
-    
-  search <- function(inits, lines, cutoff) {
-    r <- inits$r0
-    d <- inits$d0
-    step <- inits$step0
-    
-    if(d < cutoff) return(r)
-    
-    r1 <- r + step
-    theta1 <- sample_sphere(space = ncol(stat), n, r1)
+  d0 <- exp_vals0 %>% t() %>%
+    data.frame() %>%
+    rowwise() %>% 
+    transmute_(P=paste0("paste(",paste(rownames(exp_vals0), collapse=","),", sep=',')")) %>%
+    merge(triangles) %>%    
+    select(-triangle) %>%
+    ungroup() %>%
+    apply(1, function(x) distance_to_simplex(P=x["P"], simplex=as.list(x[c("point1", "point2", "point3")]))) %>%
+    min()
+ 
+
+
+  success <- FALSE
+  while(!success){
+
+    r1 <- r0 + step
+    theta1 <- sample_sphere(stat, n, r1)
     exp_vals1 <- expected_value(stats = stat, theta = theta1, normalized = TRUE)
-    d1 <- min(apply(exp_vals1, 2, function(x) min_distance_to_hull(x, lines)))
+    d1 <- exp_vals1 %>% t() %>%
+      data.frame() %>%
+      rowwise() %>% 
+      transmute_(P=paste0("paste(",paste(rownames(exp_vals0), collapse=","),", sep=',')")) %>%
+      merge(triangles) %>%    
+      select(-triangle) %>%
+      ungroup() %>%
+      apply(1, function(x) distance_to_simplex(P=x["P"], simplex=as.list(x[c("point1", "point2", "point3")]))) %>%
+      min()
+
+    if(d0 < cutoff & d1 >= cutoff) success <- TRUE
     
-    r2 <- r1 - step/2
-    theta2 <- sample_sphere(space = ncol(stat), n, r2)
-    exp_vals2 <- expected_value(stats = stat, theta = theta2, normalized = TRUE)
-    d2 <- min(apply(exp_vals2, 2, function(x) min_distance_to_hull(x, lines)))
-  
-    if(d1 >= cutoff) {
-      search(list(r0=r1, d0=d1, step0=step), lines, cutoff)
+    if(d1 < cutoff) {
+      r0 <- r1
+      d0 <- d1
+      step <- -backstep
     } else {
-      search(list(r0=r2, d0=d2, step0=step/2), lines, cutoff)
-    } 
+      r0 <- r1
+      d0 <- d1
+    }
+    cat(d0)
+
   }
-  
-  search(list(r0=r0, d0=d0, step0=step0), lines, .001)
+  return(r0)
 }
 
 
