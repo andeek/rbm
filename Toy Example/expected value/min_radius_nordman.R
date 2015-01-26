349bfbacffca0eff60fb4d7b568d60b4da86e429
Andee Kaplan <andeek@erichare.student.iastate.edu>
2015-01-26 10:49:43 -0600

    nordman method working

--- Toy Example/expected value/min_radius_nordman.R
+++ Toy Example/expected value/min_radius_nordman.R
@@ -2,28 +2,65 @@ source("functions.R")
 library(dplyr)
 library(tidyr)
 
-H <- 1
-V <- 2
-
-stat <- stats(H, V, "negative")
 n <- 100
 r_center <- 2
 
-theta <- sample_sphere(stat, n, r_center)
-g_theta <- t(expected_value(theta, stat))
+test_cases <- expand.grid(data.frame(1:4)[,rep(1,2)]) %>% 
+  rename(H = X1.4, V = X1.4.1) %>%
+  filter(H <= V) %>% #remove those cases with less visibles than hiddens. Is this necessary?
+  mutate(n_param = H*V + H + V) %>%
+  mutate(max_facets = (2^(H+V))^(floor(n_param/2))) %>%
+  mutate(n = n, r = r_center) %>%
+  group_by(H, V, n_param, n, r) %>%
+  do(stat = stats(.$H, .$V, "negative")) %>%
+  ungroup() %>%
+  group_by(H, V, n_param, n, r) %>%
+  do(g_theta = t(expected_value(theta = sample_sphere(.$stat[[1]], .$n, .$r), stats = data.matrix(.$stat[[1]]))))
 
-r_exp <- .015
+test_cases_stat <- expand.grid(data.frame(1:4)[,rep(1,2)]) %>% 
+  rename(H = X1.4, V = X1.4.1) %>%
+  filter(H <= V) %>% #remove those cases with less visibles than hiddens. Is this necessary?
+  mutate(n_param = H*V + H + V) %>%
+  mutate(max_facets = (2^(H+V))^(floor(n_param/2))) %>%
+  mutate(n = n, r = r_center) %>%
+  group_by(H, V, n_param, n, r) %>%
+  do(stat = stats(.$H, .$V, "negative"))
 
-apply(g_theta, 1, function(x){
-  x %>%
-    sample_points_outside(stat, r_exp) %>%
-    filter(!in_hull) %>%
-    nrow() -> got_out
-  res <- ifelse(got_out > 0, r_exp, NA)   
-})
+cases <- inner_join(test_cases, test_cases_stat)
 
+cases %>%
+  group_by(H, V) %>%
+  do(min_dist = find_min_dist(.$g_theta[[1]], .$stat[[1]], .05, .005)) %>%
+  mutate(min_dist = as.numeric(min_dist))-> dists
 
-
+find_min_dist <- function(g_theta, stat, step, backstep) {
+  r_exp_0 <- 0
+  num_0 <- 0
+  
+  success <- FALSE
+  while(!success) {
+    r_exp_1 <- r_exp_0 + step
+    
+    for(i in 1:nrow(g_theta)) {
+      g_theta[i,] %>%
+        sample_points_outside(stat, r_exp_1) %>%
+        filter(!in_hull) %>%
+        dim() -> num_1
+      if(num_1[1] > 0) break
+    }
+    
+    if(num_1[1] == 0 & num_0 > 0) success <- TRUE
+    
+    r_exp_0 <- r_exp_1
+    num_0 <- num_1[1]
+    
+    if(num_1[1] > 0) {
+      step <- -backstep
+    }
+  } 
+  
+  return(r_exp_0)
+}
 
 sample_points_outside <- function(g_theta, stats, r, n = 100) {
   #g_theta is the expected value mapping of a point theta in R^H+V+H*V
@@ -32,7 +69,7 @@ sample_points_outside <- function(g_theta, stats, r, n = 100) {
         separate(col = vals, sep = ",", into = colnames(stats)) %>%
         select(-samp)
      
-  x0$in_hull <- apply(x0, 1, in_hull, stat)
+  x0$in_hull <- apply(x0, 1, in_hull, stats)
 
   return(x0)
 }
