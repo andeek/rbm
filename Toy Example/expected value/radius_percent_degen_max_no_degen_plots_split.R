1fb8e906bc5cf8112ac22b912386700588b7d0fa
Andee Kaplan <ajkaplan@iastate.edu>
2015-03-22 10:25:22 -0500

    kaiser method and split method

--- Toy Example/expected value/radius_percent_degen_max_no_degen_plots_split.R
+++ Toy Example/expected value/radius_percent_degen_max_no_degen_plots_split.R
@@ -1,5 +1,4 @@
 library(ggplot2)
-library(plyr)
 library(dplyr)
 library(tidyr)
 
@@ -13,12 +12,19 @@ plot_data <- function(res) {
     
     tmp %>% 
       data.frame() %>% 
-      rowwise() %>% 
+      rowwise() %>%
       mutate_(ss_ratio = paste0("(", paste(paste0(names(.)[(H + V + 1):(H + V + H*V)], "^2"), collapse = " + "), ")/sum(", paste(paste0(names(.)[1:(H+V)],"^2"), collapse = " + "), ")")) %>%    
       ungroup() -> ratio
     
-    inner_join(ratio, res$outside[[i]] %>% ungroup()) %>%
-      select(ss_ratio, near_hull) %>%
+    tmp %>%
+      data.frame() %>%
+      select(-starts_with("exp")) %>%
+      rowwise() %>%
+      mutate_(true_r = paste0("sqrt(", paste(paste0(names(.), "^2"), collapse = " + "), ")")) -> radius
+      
+    inner_join(radius, ratio) %>%
+    inner_join(res$outside[[i]] %>% ungroup()) %>%
+      select(ss_ratio, near_hull, true_r) %>%
       mutate(H = H, V = V, n_param = H + V + H*V) %>%
       rbind(plot.data) -> plot.data
   }
@@ -32,11 +38,12 @@ r_exp <- .05
 
 plot.dat <- list()
 for(i in seq(0, 1, by = 0.1)) {
-  load(paste0("apps/results_", i, ".RData")) 
+  load(paste0("apps/results_split_", i, ".RData")) 
   plot_dat <- lapply(res, plot_data)
   names(plot_dat) <- gsub("[.]", "_", r_center)
   
   do.call(rbind, plot_dat) %>%
+    ungroup() %>%
     mutate(name = rownames(do.call(rbind, plot_dat))) %>%
     separate(name, into = c("multiplier", "num"), "[.]") %>% 
     mutate(multiplier = as.numeric(gsub("_", ".", multiplier))) %>%
@@ -47,23 +54,38 @@ for(i in seq(0, 1, by = 0.1)) {
 
 names(plot.dat) <- gsub("[.]", "_", seq(0, 1, by = 0.1))
 do.call(rbind, plot.dat) %>%
-  mutate(name2 = rownames(do.call(rbind, plot.dat))) %>%
+  mutate(name2 = rownames(do.call(rbind, plot.dat))) %>% 
   separate(name2, into = c("power", "num2"), "[.]") %>% 
   mutate(power = as.numeric(gsub("_", ".", power))) -> plot.dat
 
 
 plot.dat %>%
-  mutate(radius = multiplier*(H + V)^power) %>%
-  group_by(radius, n_param) %>%
+  group_by(true_r, n_param) %>%
   summarise(frac_degen = sum(near_hull)/n()) %>%
   filter(frac_degen < .05) %>% 
   group_by(n_param) %>%
-  summarise(best_radius = max(radius)) -> best_rad_nodegen
+  summarise(best_radius = max(true_r)) -> best_rad_nodegen
 
 best_rad_nodegen %>%
   ggplot() +
   geom_point(aes(n_param, best_radius)) +
   geom_smooth(aes(n_param, best_radius), method = "lm") +
-  geom_smooth(aes(n_param, best_radius), method = "loess", colour = "red")
+  theme_bw(base_family = "serif") +
+  theme(legend.position = "bottom")
 
-lm(best_radius ~ n_param, data = best_rad_nodegen) %>% plot()
+plot.dat %>%
+  group_by(true_r, n_param_f) %>%
+  summarise(frac_degen = sum(near_hull)/n()) %>%
+  ggplot() +
+  geom_point(aes(true_r, frac_degen, colour = n_param_f)) +
+  theme_bw(base_family = "serif") +
+  theme(legend.position = "bottom")
+
+
+
+plot.dat %>%
+  group_by(true_r, n_param) %>%
+  summarise(frac_degen = sum(near_hull)/n()) %>%
+  mutate(happy = frac_degen < .05) %>%
+  group_by(n_param, happy) %>%
+  summarise(count = n())
--- Toy Example/expected value/radius_percent_degen_split.R
+++ Toy Example/expected value/radius_percent_degen_split.R
@@ -1,5 +1,4 @@
-## load libraries ---------------------------
-
+## load libraries --------------------------- 
 source("functions.R")
 library(dplyr)
 library(tidyr)
@@ -52,6 +51,46 @@ in_hull <- function(point, hull_points) {
   return(solve(lp_obj) == 0)
 }
 
+sample_sphere_main <- function(stat, n, r = 1) {
+  require(dplyr)
+  require(tidyr)
+  
+  samp <- function(space, r) {
+    vec <- rnorm(space)
+    paste0(r*vec/sqrt(sum(vec^2)), collapse=",")
+  }
+  
+  res <- data.frame(samp = 1:n) %>%
+    group_by(samp) %>%
+    mutate(vals = samp(ncol(stat %>% data.frame() %>% select(-contains("theta"))), r)) %>%
+    separate(col = vals, sep = ",", into = colnames(stat %>% data.frame() %>% select(-contains("theta")))) %>%
+    select(-samp) %>%
+    data.matrix() %>%
+    t()
+  
+  return(res)  
+}
+
+sample_sphere_interact <- function(stat, n, r = 1) {
+  require(dplyr)
+  require(tidyr)
+  
+  samp <- function(space, r) {
+    vec <- rnorm(space)
+    paste0(r*vec/sqrt(sum(vec^2)), collapse=",")
+  }
+  
+  res <- data.frame(samp = 1:n) %>%
+    group_by(samp) %>%
+    mutate(vals = samp(ncol(stat %>% data.frame() %>% select(contains("theta"))), r)) %>%
+    separate(col = vals, sep = ",", into = colnames(stat %>% data.frame() %>% select(contains("theta")))) %>%
+    select(-samp) %>%
+    data.matrix() %>%
+    t()
+  
+  return(res)  
+}
+
 ## params ----------------------------------
 n <- 100
 r_center <- seq(0.2, 3, by = 0.2)
@@ -67,39 +106,46 @@ get_res <- function(n, r_center, r_exp, power) {
     filter(H <= V) %>% #remove those cases with less visibles than hiddens. Is this necessary?
     mutate(n_param = H*V + H + V) %>%
     mutate(max_facets = (2^(H+V))^(floor(n_param/2))) %>%
-    mutate(n = n, r = r_center*(H + V)^(power)) %>%
-    group_by(H, V, n_param, n, r) %>%
+    mutate(n = n, r1 = r_center*(H + V), r2 = r_center*(H + V)^(power)) %>%
+    group_by(H, V, n_param, n, r1, r2) %>%
     do(stat = stats(.$H, .$V, "negative")) %>%
     ungroup() %>%
-    group_by(H, V, n_param, n, r) %>%
-    do(samp = sample_sphere(.$stat[[1]], .$n, .$r))
+    group_by(H, V, n_param, n, r1, r2) %>%
+    do(samp = rbind(sample_sphere_main(.$stat[[1]], .$n, .$r1), sample_sphere_interact(.$stat[[1]], .$n, .$r2)))
+    
+  test_cases %>%
+    left_join(test_cases %>%
+                group_by(H, V, n_param, n, r1, r2) %>%
+                do(true_r = apply(.$samp[[1]], 2, function(x) sqrt(sum(x^2))))) -> test_cases
+    
   
   test_cases_stat <- expand.grid(data.frame(1:4)[,rep(1,2)]) %>% 
     rename(H = X1.4, V = X1.4.1) %>%
     filter(H <= V) %>% #remove those cases with less visibles than hiddens. Is this necessary?
     mutate(n_param = H*V + H + V) %>%
     mutate(max_facets = (2^(H+V))^(floor(n_param/2))) %>%
-    mutate(n = n, r = r_center*(H + V)^(power)) %>%
-    group_by(H, V, n_param, n, r) %>%
+    mutate(n = n, r1 = r_center*(H + V), r2 = r_center*(H + V)^(power)) %>%
+    group_by(H, V, n_param, n, r1, r2) %>%
     do(stat = stats(.$H, .$V, "negative"))
   
   test_cases <- inner_join(test_cases, test_cases_stat)
   
   test_cases %>%
-    group_by(H, V, n_param, n, r) %>%
+    group_by(H, V, n_param, n, r1, r2) %>%
     do(g_theta = t(rbind(.$samp[[1]], expected_value(theta = .$samp[[1]], stats = data.matrix(.$stat[[1]]))))) -> cases
   
   cases <- inner_join(cases, test_cases)
   
   cases %>%
-    group_by(H, V, r) %>%
+    group_by(H, V, r1, r2) %>%
     do(outside = find_prop(.$g_theta[[1]] %>% data.frame() %>% select(starts_with("exp")), .$stat[[1]], r_exp)) -> tmp
   
-  res <- inner_join(tmp, cases %>% group_by(H, V, r, n_param) %>% do(samp = .$g_theta[[1]])) 
+  res <- inner_join(tmp, cases %>% group_by(H, V, r1, r2, n_param) %>% do(samp = .$g_theta[[1]]))
 }
 
 ## run simulations -------------------------------------
 for(i in power) {
+  cat(paste("power:", i))
   res <- lapply(r_center, function(r) get_res(n, r, r_exp, i))
-  save(res, file = paste0("results_", i, ".RData"))
+  save(res, file = paste0("apps/results_split_", i, ".RData"))
 }
