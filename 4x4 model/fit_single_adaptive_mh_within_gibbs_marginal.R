365d0635c4db610590500ef3710d69b033ae124b
Andee Kaplan <ajkaplan@iastate.edu>
2015-12-03 21:02:20 -0600

    running marginal model

--- 4x4 model/fit_single_adaptive_mh_within_gibbs_marginal.R
+++ 4x4 model/fit_single_adaptive_mh_within_gibbs_marginal.R
@@ -7,7 +7,7 @@ source("sample_functs.R")
 #data and params ------------------------
 H <- 4
 V <- 4
-mc.iter <- 5000
+mc.iter <- 2000
 set.seed(102285) #reproducible seed
 
 load("written/sample_images.Rdata")
@@ -29,9 +29,9 @@ trunc_const <- 1
 
 models_good <- sample_single_adaptive_mh_within_gibbs(visibles = flat_images_good$visibles, params0 = params, C = variance_params$C, C_prime =  variance_params$C_prime, trunc_const = trunc_const, h = h, s1 = s1, s2 = s2, mc.iter = mc.iter, conditional_function = log_conditional_marginal)
 models_bad <- sample_single_adaptive_mh_within_gibbs(visibles = flat_images_good$visibles, params0 = params, C = variance_params$C, C_prime = variance_params$C, trunc_const = trunc_const, h = h, s1 = s1, s2 = s2, mc.iter = mc.iter, conditional_function = log_conditional_marginal)
-models_degen <- sample_single_adaptive_mh_within_gibbs(visibles = flat_images_degen$visibles, params0 = params, C = variance_params$C, C_prime = variance_params$C_prime, trunc_const = trunc_const, h = h, s1 = s1, s2 = s2, mc.iter = mc.iter, conditional_function = log_conditional_marginal)
+#models_degen <- sample_single_adaptive_mh_within_gibbs(visibles = flat_images_degen$visibles, params0 = params, C = variance_params$C, C_prime = variance_params$C_prime, trunc_const = trunc_const, h = h, s1 = s1, s2 = s2, mc.iter = mc.iter, conditional_function = log_conditional_marginal)
 
-save(models_good, models_bad, models_degen, file = "written/fitted_models_adaptive_mh_full_trunc_distn_marginal.Rdata")
+save(models_good, models_bad, file = "written/fitted_models_adaptive_mh_full_trunc_distn_marginal.Rdata")
 
 #burnin + thinning
 burnin_thinning <- function(models) {
@@ -47,6 +47,6 @@ burnin_thinning <- function(models) {
 
 models_good <- burnin_thinning(models_good)
 models_bad <- burnin_thinning(models_bad)
-models_degen <- burnin_thinning(models_degen)
+#models_degen <- burnin_thinning(models_degen)
 
-save(models_good, models_bad, models_degen, file = "written/fitted_models_adaptive_mh_trunc_distn_marginal.Rdata")
+save(models_good, models_bad, file = "written/fitted_models_adaptive_mh_trunc_distn_marginal.Rdata")
