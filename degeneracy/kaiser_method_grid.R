312e5260f1103514f338209b736d30c2eab3c8fc
Andee Kaplan <ajkaplan@iastate.edu>
2016-02-19 21:08:50 -0600

    good progress on poster

--- degeneracy/kaiser_method_grid.R
+++ degeneracy/kaiser_method_grid.R
@@ -20,15 +20,29 @@ res %>%
 
 exp_vals %>%
   group_by(H, V, N, r1, r2) %>%
-  do(data.frame(mean_abs_diff = mean(abs(.$indep_exp[[1]] - .$marg_exp[[1]])))) %>%
+  do(data.frame(mean_abs_diff = apply(abs(.$indep_exp[[1]] - .$marg_exp[[1]]), 1, mean))) %>%
+  group_by(H, V, N, r1, r2) %>%
+  summarise(mean_abs_diff = mean(mean_abs_diff)) %>%
   mutate(Hiddens = paste0("Hiddens: ", H), Visibles = paste0("Visibles: ", V)) %>%
   ggplot() +
   geom_tile(aes(x = r1, y = r2, fill = mean_abs_diff)) +
   geom_contour(aes(x = r1, y = r2, z = mean_abs_diff), colour = "black", bins = 8) +
-  #geom_contour(aes(x = r1, y = r2, z = frac_degen), colour = "black", breaks = .05, size = 1.5) +
+  #geom_contour(aes(x = r1, y = r2, z = mean_abs_diff), colour = "black", breaks = .1, size = 1.5) +
   #geom_abline(aes(intercept = 0, slope = 1), alpha = .5, lty = 2) +
   scale_fill_gradient("Mean absolute difference in expectation", low = "yellow", high = "red") +
   facet_grid(Visibles~Hiddens) +
   xlab(expression(group("||", theta[main], "||"))) +
   ylab(expression(group("||", theta[interaction], "||"))) +
   theme(aspect.ratio = 1)
+
+exp_vals %>% 
+  mutate(dist = sqrt(r1^2 + r2^2)) %>%
+  group_by(dist, H, V) %>%
+  do(data.frame(mean_abs_diff = mean(abs(.$indep_exp[[1]] - .$marg_exp[[1]])))) %>%
+  mutate(Hiddens = as.character(H), Visibles = paste0("Visibles: ", V)) %>%
+  ggplot() +
+  geom_point(aes(dist, mean_abs_diff, colour = Hiddens)) + ylim(c(0, .0025))
+  #geom_smooth(aes(dist, mean_abs_diff, colour = Hiddens, group = Hiddens)) +
+  facet_grid(~Visibles) +
+  xlab(expression(paste(group("||", theta, "||"), ", Distance from the origin"))) +
+  ylab("Mean absolute difference in expectation")
